12c5d2ffe5e52594abc4ec98b6c239ee917cfcca
Thiruvanth J P <thiruvanthjp@gmail.com>
2024-07-06 22:24:28 +0530

    changed phone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,9 @@
-/*
-export default function Contact () {
 
-    const config = {
-        email : 'thiruvanthjp@gmail.com',
-        phone: '+91 9514785523'
-    }
-
-    return <section id='contact' className='flex flex-col bg-primary px-5 py-32 text-white'>
-        <div className='flex flex-col items-center'>
-           
-            <h1 className='text-4xl border-b-4  border-secondary mb-5 w-[140px] font-bold'>Contact</h1>
-            <p className='pb-5'>If you want to discuss more in detail, feel free contact me</p>
-            <p className='py-2'><span className='font-bold'>Email :</span> {config.email}</p>
-            <p className='py-2'><span className='font-bold'>Phone :</span> {config.phone}</p>
-        </div>
-    </section>
-}
-    */
 export default function Contact() {
 
     const config = {
         email: 'thiruvanthjp@gmail.com',
-        phone: '+91 9514785523'
+        phone: '+91 9514785522'
     }
 
     return (
