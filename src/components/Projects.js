08502d78cca5d81689c0f157d6371acc87527f79
Thiruvanth J P <thiruvanthjp@gmail.com>
2024-11-29 21:16:20 +0530

    project-update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ export default function Project() {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-10 px-18">
                     <div className="flex flex-col items-center text-center group">
                         <img className="w-64 h-64 object-cover mb-4" src={websiteImg1} alt="Project 1"/>
-                        <h2 className="font-bold text-lg">OpenAI's DALL-E Clone</h2>
+                        <h2 className="font-bold text-lg">ImageGen : Advanced AI Platform</h2>
                         <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-1">
                             <p>A Full-stack application of OpenAI's DALL-E, a Text to Image-AI application utilizing Mongo-DB,Express.js,React JS,Node.js (MERN) Web stack.</p>
                             <p className='py-2'>Utilized OpenAI's API, enabling users to generate images and share them to the community.</p>
@@ -24,7 +24,7 @@ export default function Project() {
                     </div>
                     <div className="flex flex-col items-center text-center group">
                         <img className="w-64 h-64 object-cover mb-4" src={websiteImg2} alt="Project 2"/>
-                        <h2 className="font-bold text-lg">BERT Transformer - NLP Project</h2>
+                        <h2 className="font-bold text-lg">BERT Powered Sentiment Analysis</h2>
                         <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-1">
                             <p>This project explores the efficacy of leveraging BERT, a Transformer model for Natural Language Processing.</p>
                             <p className='py-2'>Utilized LIME and SHAP tools for Explainable AI interpretations.</p>
@@ -35,11 +35,11 @@ export default function Project() {
                         </div>
                     </div>
                     <div className="flex flex-col items-center text-center group">
-                        <img className="w-64 h-64 object-cover mb-4" src={websiteImg3} alt="Project 3"/>
-                        <h2 className="font-bold text-lg">Chat App and other Projects in JAVA</h2>
+                        <img className="w-64 h-64 object-cover mb-3" src={websiteImg3} alt="Project 3"/>
+                        <h2 className="font-bold text-lg">Role-Based Access Control using Node.js & JWT</h2>
                         <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-1">
-                            <p>A real-time chat application that allows users to send and receive messages over a network utilizing TCP.</p>
-                            <p className='py-2'>Other applications were built using Java's capabilities for communication and utilizing Swing for the GUI.</p>
+                            <p>This Project is a backend system built with Node.js, Express.js, and MongoDB to manage user roles and permissions.</p>
+                            <p className='py-2'>It uses JSON Web Tokens-JWT for secure authentication, ensuring controlled access based on roles. This project highlights skills in backend development, API security, and role-based access management.</p>
                             <div className="mt-2">
                                 <a className='btn text-white font-normal bg-gray-500 hover:bg-gray-600' href="https://github.com/thiruvanth04/Java-Applications" target="_blank" rel="noopener noreferrer">View Project</a>
                             </div>
