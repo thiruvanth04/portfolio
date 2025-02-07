1baabdc85a321708b1d6689d800a2676cd0b55ae
Thiruvanth J P <thiruvanthjp@gmail.com>
2025-02-07 21:55:54 +0530

    resume update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,7 +11,7 @@ export default function Resume () {
                 <div className='flex flex-col justify-center text-white'>
                     <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold hover:scale-105 text-center'>Resume</h1>
                     <p className='pb-5'>You can View & Download my resume</p>
-                    <a className='btn2' href='https://drive.google.com/file/d/1mTOUAvGjUGqyAw1677f6dj_wKZjukPIO/view?usp=sharing' target='_blank' rel='noopener noreferrer'> Download</a>
+                    <a className='btn2' href='https://drive.google.com/file/d/1t1Um9WZ3IIy8C102KlexGmVwz3IezT6n/view?usp=sharing' target='_blank' rel='noopener noreferrer'> Download</a>
                 </div>
             </div>
         </section>
